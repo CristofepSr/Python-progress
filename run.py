30a5699212e53091a96cbf7c421b067a24cef374
Cristofep <srcristofep@gmail.com>
2024-03-15 13:54:46 -0400

    mejorando la potencia

--- run.py
+++ run.py
@@ -49,19 +49,19 @@ def divicion(num1, num2):#Hecho por cristofep
     return f'<center><h1>{num1} ÷ {num2} = {resul}</h1></center>'
 
 # Divicion
-@app.route('/potencia/<num1>/<num2>')
-@app.route('/potencia/<num1>/<num2>/<num3>')
-@app.route('/potencia/<num1>/<num2>/<num3>/<num4>')
-def potencia(num1, num2, num3 ,num4):#Hecho por cristofep
-    if num4 == None and num3 == None:
-        resul = num1 ** num2
-        return f'<center><h1>{num1} ^ {num2} = {resul}</h1></center>'
-    elif num4 == None:
-        resul = num1 ** num2 ** num3
-        return f'<center><h1>{num1} ^ {num2} ^ {num3} = {resul}</h1></center>'
+@app.route('/potencia/base>/<num1>')
+@app.route('/potencia/<base>/<num1>/<num2>')
+@app.route('/potencia/<base>/<num1>/<num2>/<num3>')
+def potencia(base, num1, num2 ,num3):#Hecho por cristofep
+    if num3 == None and num2 == None:
+        resul = base ** num1
+        return f'<center><h1>{base} ^ {num1} = {resul}</h1></center>'
+    elif num3 == None:
+        resul = base ** num1 ** num2
+        return f'<center><h1>{base} ^ {num1} ^ {num2} = {resul}</h1></center>'
     else: #Hecho por cristofep
-        resul = num1 ** num2 ** num3 ** num4
-        return f'<center><h1>{num1} ^ {num2} ^ {num3} ^ {num4} = {resul}</h1></center>'
+        resul = base ** num1 ** num2 ** num3
+        return f'<center><h1>{base} ^ {num1} ^ {num2} ^ {num3} = {resul}</h1></center>'
 
 if __name__ == '__main__':
     app.run(port=5500, debug=True )#Hecho por cristofep
