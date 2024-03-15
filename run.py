ae8ceffef4797b01be88ce3433e745a2e3cd265d
Cristofep <srcristofep@gmail.com>
2024-03-15 09:30:40 -0400

    Operacuinde multiplicacion

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def index():
     return '<center><h1></h1></center>'
 
+# Suma
 @app.route('/suma/<int:num1>/<int:num2>') #suma de dos numeros
 @app.route('/suma/<int:num1>/<int:num2>/<int:num3>') #suma de tres numeros
 @app.route('/suma/<int:num1>/<int:num2>/<int:num3>/<int:num4>') #suma de cuatro numeros
@@ -20,6 +21,7 @@ def suma(num1, num2, num3 = None, num4 = None):
         resul = num1 + num2 + num3 + num4
         return f'<center><h1>{num1} + {num2} + {num3} + {num4} = {resul}</h1></center>'
 
+# Resta
 @app.route('/resta/<int:num1>/<int:num2>') #Resta de dos numeros
 @app.route('/resta/<int:num1>/<int:num2>/<int:num3>') #Resta de tres numeros
 @app.route('/resta/<int:num1>/<int:num2>/<int:num3>/<int:num4>') #Resta de cuatro numeros
@@ -33,6 +35,13 @@ def resta(num1, num2, num3 = None, num4 = None):
     else:
         resul = num1 - num2 - num3 - num4
         return f'<center><h1>{num1} - {num2} - {num3} - {num4} = {resul}</h1></center>'
+
+# Multiplicancion
+@app.route('/multiplicacion/<num1>/<num2>')
+def multiplicacion(num1, num2):
+    resul = num1 * num2
+    return f'<center><h1>{num1} x {num2} = {resul}</h1></center>'
+
 
 if __name__ == '__main__':
     app.run(port=5500, debug=True )
