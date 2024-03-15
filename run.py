8bec3d6eb3d8ddd5caa1eae85a9f494599df5632
Cristofep <srcristofep@gmail.com>
2024-03-15 07:46:06 -0400

    Operaciones de resta de 2,3 y 4 numeros

--- run.py
+++ run.py
@@ -20,5 +20,19 @@ def suma(num1, num2, num3 = None, num4 = None):
         resul = num1 + num2 + num3 + num4
         return f'<center><h1>{num1} + {num2} + {num3} + {num4} = {resul}</h1></center>'
 
+@app.route('/resta/<int:num1>/<int:num2>') #Resta de dos numeros
+@app.route('/resta/<int:num1>/<int:num2>/<int:num3>') #Resta de tres numeros
+@app.route('/resta/<int:num1>/<int:num2>/<int:num3>/<int:num4>') #Resta de cuatro numeros
+def resta(num1, num2, num3 = None, num4 = None):
+    if num4 == None and num3 == None:
+        resul = num1 - num2
+        return f'<center><h1>{num1} - {num2} = {resul}</h1></center>'
+    elif num4 == None:
+        resul = num1 - num2 - num3
+        return f'<center><h1>{num1} - {num2} - {num3} = {resul}</h1></center>'
+    else:
+        resul = num1 - num2 - num3 - num4
+        return f'<center><h1>{num1} - {num2} - {num3} - {num4} = {resul}</h1></center>'
+
 if __name__ == '__main__':
     app.run(port=5500, debug=True )
